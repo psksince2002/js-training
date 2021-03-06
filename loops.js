88c439191226384e0e4cbcc40c7e445be528e380
psksince2002 <saikoushik1978@gmail.com>
2021-03-06 20:37:06 +0530

    loops

---
+++ loops.js
@@ -0,0 +1,16 @@
+var a=[10,20,30,40,60,70,80]
+
+i=0
+while(i<a.length){
+    console.log(a[i])
+    i++;
+}
+
+for(i=0;i<a.length;i++){
+    console.log(a[i])
+}
+
+for( i of a){
+    console.log(i)
+}
+
