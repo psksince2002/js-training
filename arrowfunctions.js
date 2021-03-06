f3890a1889a0eab7eedd0519d4e9fb91ac770ab5
psksince2002 <saikoushik1978@gmail.com>
2021-03-06 19:19:51 +0530

    arrowfunctions

---
+++ arrowfunctions.js
@@ -0,0 +1,12 @@
+//arrow functions are advanced anonymous functions
+
+let test=(a)=>{
+      console.log('arrow functions are cool')
+}
+
+test(2)
+
+//only one statement and if it is a return statement
+let test2=(a)=>(a*a)
+
+console.log(test2(5))
