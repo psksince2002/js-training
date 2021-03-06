f1e61f169c73cd6452f8b84daa75d79cd23488fa
psksince2002 <saikoushik1978@gmail.com>
2021-03-06 19:05:00 +0530

    opeartors

---
+++ operators.js
@@ -0,0 +1,23 @@
+/*
+
+operators in js
+
++,-,*,/,%,++,--,**,=,==,!=,===,!===,>,?:,&&,||,!
+
+*/
+
+var a=2
+var b=3
+
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+console.log(a%b)
+console.log(a++)
+console.log(b--)
+console.log(a**b)
+var str="2"
+var a=2
+console.log(a==str)
+console.log(a===str)
