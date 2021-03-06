a40ab9c2c04cf8dfd5dd2b7bfa65c22f96be8824
psksince2002 <saikoushik1978@gmail.com>
2021-03-06 19:09:44 +0530

    datatypes

---
+++ datatypes.js
@@ -0,0 +1,18 @@
+/* datatypes
+in js
+primitive
+boolean,number,string,null,undefined
+reference
+object,arrays,functions*/
+
+var a=true;
+console.log(typeof(a))
+var a=23;
+console.log(typeof(a))
+var a="faction"
+console.log(typeof(a))
+var a=null;
+console.log(typeof(a))
+var a=undefined
+console.log(typeof(a))
+
